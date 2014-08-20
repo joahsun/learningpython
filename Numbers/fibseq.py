4590cafe7325f426d2cd193f419bda598aa64fc2
sun.haixuan <sunhaixuan@gamil.com>
2014-08-20 09:57:40 +0800

    New way

--- Numbers/fibseq.py
+++ Numbers/fibseq.py
@@ -5,21 +5,14 @@ __author__ = 'sun.haixuan'
 
 def gen_fibo(num):
     assert (num >= 0)
-    a, b = 0, 1
-    n = 0
-    while 1:
-        tmp = a + b
-        a = b
-        b = tmp
-        n += 1
-        if n == num:
-            print(a)
-            break
-        print(a, end=',')
+    seq = [0, 1]
+    for n in range(2, num):
+        seq.append(seq[n-2] + seq[n-1])
+    print(','.join(map(str, seq[:num])))
 
 
 def main():
-    gen_fibo(int(input("How many numbers:")))
+    gen_fibo(int(raw_input("How many numbers:")))
 
 
 if __name__ == '__main__':
